61947610df6da500528791c16d593cbea8fc3c0d
jw20191n <clairejw1015@gmail.com>
2019-10-22 17:37:28 -0400

    pokemon info show when click card

---
+++ pokemon_app/app/assets/javascripts/info.js
@@ -0,0 +1,59 @@
+//newContainer pokemonInfo is defined in index.js
+const pokemonInfo = document.getElementById('pokemonInfo');
+
+newContainer.addEventListener('click', () => {
+    console.log("clicked");
+    let target = event.target;
+    if (target.classList.contains('pokeCard')){
+        let cardId = target.getAttribute('data-id');
+        getPokemonInfo(cardId);
+    }
+});
+
+function getPokemonInfo(cardId){
+    fetch(`http://localhost:3000/pokemons/${cardId}`)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+        printInfo(data);
+    })
+}
+
+function printInfo(data){
+    pokemonInfo.innerHTML = `
+        <h3>Name: ${data.name}</h3>
+        <h5>Type: ${data.poke_type}</h5>
+        <h5>Move: ${data.move}</h5>
+        <p>Likes: ${data.likes} </p>
+        <button class="btn">like</button> 
+    ` 
+    pokemonInfo.setAttribute('data-id', data.id);
+}
+
+pokemonInfo.addEventListener('click', ()=>{
+    let target = event.target.className;
+    if(target === 'btn'){
+       let likeString = event.target.parentNode.querySelector('p').innerText;
+       let likeArray = likeString.split(' ');
+       let number = parseInt(likeArray[1]);
+       number += 1;
+       event.target.parentNode.querySelector('p').innerText = `Likes: ${number}`;
+       let tempId = event.target.parentNode.getAttribute('data-id');
+       incrementLike(tempId, number);
+    }
+});
+
+function incrementLike(tempId, number){
+    fetch(`http://localhost:3000/pokemons/${tempId}`, {
+        method: 'PATCH',
+        header: {
+            'content-type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            likes: number
+        })
+    }).then(resp => {
+        return resp.json()
+    })
+}
