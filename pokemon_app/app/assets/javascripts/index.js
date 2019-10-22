320186f53f8c87c6f3555a2427b085efceed6132
jw20191n <clairejw1015@gmail.com>
2019-10-22 14:45:43 -0400

    ??

--- pokemon_app/app/assets/javascripts/index.js
+++ pokemon_app/app/assets/javascripts/index.js
@@ -1,39 +1,39 @@
-// let scene, camera, renderer;
+let scene, camera, renderer;
 const container = document.getElementById('container');
 const newContainer = document.querySelector('.glider');
 
-// function init(){
-//     scene = new THREE.Scene();
-//     camera = new THREE.PerspectiveCamera(75, window.innerWidth/innerHeight, 0.1, 1000);
-//     scene.background = new THREE.Color(0xdddddd);
-//     camera.position.z = 5;
-//     renderer = new THREE.WebGLRenderer({antialias: true, alpha: true } );
-//     renderer.setClearColor(0xff0000);
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-//     document.body.appendChild(renderer.domElement);
-//     window.addEventListener('resize', () =>{
-// 	    renderer.setSize(window.innerWidth, window.innerHeight);
-// 	    camera.aspect = window.innerWidth/innerHeight;
-//         camera.updateProjectionMatrix();
-//         })
-//     light = new THREE.PointLight(0xFFFFFF, 1, 20);
-//     light.position.set(1,1,1);
-//     scene.add(light);
-//     renderer = new THREE.WebGLRenderer({antialias:true});
-//     renderer.setSize(window.innerWidth,window.innerHeight);
-//     document.body.appendChild(renderer.domElement);
-//     console.log('hey man')
+function init(){
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/innerHeight, 0.1, 1000);
+    scene.background = new THREE.Color(0xdddddd);
+    camera.position.z = 5;
+    renderer = new THREE.WebGLRenderer({antialias: true, alpha: true } );
+    renderer.setClearColor(0xff0000);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    window.addEventListener('resize', () =>{
+	    renderer.setSize(window.innerWidth, window.innerHeight);
+	    camera.aspect = window.innerWidth/innerHeight;
+        camera.updateProjectionMatrix();
+        })
+    light = new THREE.PointLight(0xFFFFFF, 1, 20);
+    light.position.set(1,1,1);
+    scene.add(light);
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    console.log('hey man')
 
-//     // var raycaster = new THREE.Raycaster();
-//     // var mouse= new THREE.Vector2();
-// }
+    // var raycaster = new THREE.Raycaster();
+    // var mouse= new THREE.Vector2();
+}
 
-// window.addEventListener('click', (event)=>{
-//     if(event.target.className === 'card-img-top'){
-//         let id = event.target.getAttribute('data-id')
-//        console.log('id: ' + id); 
-//     }
-// })
+window.addEventListener('click', (event)=>{
+    if(event.target.className === 'card-img-top'){
+        let id = event.target.getAttribute('data-id')
+       console.log('id: ' + id); 
+    }
+})
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
@@ -118,7 +118,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         
     }
 
-    // init();
+    init();
     getPokemon();
 })
 
