bd82dc937b215e841e87275de81accb8487827c6
jw20191n <clairejw1015@gmail.com>
2019-10-23 10:20:41 -0400

    change in seeds.rb

--- pokemon_app/db/seeds.rb
+++ pokemon_app/db/seeds.rb
@@ -1,8 +1,8 @@
-pikachu = Pokemon.create(name: 'Pikachu', poke_type: 'Electric', move: 'thunderbolt', image: 'https://lh3.googleusercontent.com/P5baYOsIEfhHSwFAY_8vhi5VDldzmYSqM8i7j52wkfAjJVS6F_E9zXfQmdvCBIs=w576-h432-n-rw', likes: 0, model_key: '9Apgj-wpfgb')
-bulbasaur = Pokemon.create(name: 'Bulbasaur', poke_type: 'Grass', move: 'overgrow', image: 'https://lh3.googleusercontent.com/DGcvR7S3nU-EXQUL6PytSRxrl1Oq8w2-HSocsL4OeCpiG6WhSCR9HTbnIqCSL3TT=w576-h432-n-rw', likes: 0, model_key: 'aQA7Ls8Y79f')
-charmander = Pokemon.create(name: 'Charmander', poke_type: 'Fire', move: 'blaze', image: 'https://lh3.googleusercontent.com/WTe5CADef4HfXe2awTnWiKQH6nQQOY9N05OLspPLIYgatsdfuzmZx6Mdg-TpX_sFqw=w576-h432-n-rw', likes: 0, model_key: 'd6Ar6_NHbgS')
-squirtle =  Pokemon.create(name: 'Squirtle', poke_type: 'Water', move: 'torrent', image: 'https://lh3.googleusercontent.com/mF1rIqj5nAPNEYVcQCuHDuN3UfqqLzSaWKgkn_eYrnw4UTf8HoKRoD1TDyDCS_I=w576-h432-n-rw', likes: 0, model_key: 'b9AHRGUN8jM')
-snorlax = Pokemon.create(name: 'Snorlax', poke_type: 'Normal', move: 'Gluttony', image: 'https://lh3.googleusercontent.com/Ak365NQJ-udabyKT0qMtsj53zl9ExtrmQgjFV1c6Rp4ITcSnAUaNrWOvY7c2QQ=w576-h432-n-rw', likes: 0, model_key: '2Rocc4_ltUy')
+pikachu = Pokemon.create(name: 'Pikachu', poke_type: 'Electric', move: 'thunderbolt', image: 'pokemon_app/app/assets/images/pikachu.png', likes: 0, model_key: '9Apgj-wpfgb')
+bulbasaur = Pokemon.create(name: 'Bulbasaur', poke_type: 'Grass', move: 'overgrow', image: 'pokemon_app/app/assets/images/bulbasaur.png', likes: 0, model_key: 'aQA7Ls8Y79f')
+charmander = Pokemon.create(name: 'Charmander', poke_type: 'Fire', move: 'blaze', image: 'pokemon_app/app/assets/images/charmander.png', likes: 0, model_key: 'd6Ar6_NHbgS')
+squirtle =  Pokemon.create(name: 'Squirtle', poke_type: 'Water', move: 'torrent', image: 'pokemon_app/app/assets/images/squirtle.png', likes: 0, model_key: 'b9AHRGUN8jM')
+snorlax = Pokemon.create(name: 'Snorlax', poke_type: 'Normal', move: 'Gluttony', image: 'pokemon_app/app/assets/images/pikachu.png', likes: 0, model_key: '2Rocc4_ltUy')
 jigglypuf = Pokemon.create(name: 'Jigglypuff', poke_type: 'Normal', move: 'Cute Charm', image: 'https://lh3.googleusercontent.com/P1ukarr4IKb1CaIy6ySZWjOe3_k0TBgc3W678XMhcHWToYdUNhOTSAQZ6cjgq8zMEQ=w576-h432-n-rw', likes: 0, model_key: '2yQA0j-YAj6')
 abra = Pokemon.create(name: 'Abra', poke_type: 'Psychic', move: 'Magic Guard', image: 'https://lh3.googleusercontent.com/k0KxoAAhcnfEzYFurC5MVw14nW8ciQ44VZRmbUs16mLp_bOGMO7DiWJhlZZFa68I=w576-h432-n-rw', likes: 0, model_key: 'dRT8-2o3uPO')
 diglett = Pokemon.create(name: 'Diglett', poke_type: 'Ground', move: 'Mud-Slap', image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Flh3.googleusercontent.com%2FVvV559gRMt_ryCF208UlxvCtts-JDQhkZtEgTxYJvS1Baeoc4hWspbKwoL6AYVo%3Dw576-h432-n-rw&imgrefurl=https%3A%2F%2Fpoly.google.com%2Fview%2Fd6Ar6_NHbgS&docid=IoyifWaAHEf7NM&tbnid=2mykOPcZ-mxyIM%3A&vet=10ahUKEwiMkfPY7LDlAhVxUd8KHRr0B00QMwhFKAIwAg..i&w=576&h=432&bih=678&biw=1440&q=diglett%20poly&ved=0ahUKEwiMkfPY7LDlAhVxUd8KHRr0B00QMwhFKAIwAg&iact=mrc&uact=8', likes: 0, model_key: '5PweF4YV_c6')
@@ -13,3 +13,14 @@ diglett = Pokemon.create(name: 'Diglett', poke_type: 'Ground', move: 'Mud-Slap',
 #oddish: bUYJVwEUrHD
 #haunter: e4shTQwFTFk
 #dratini bO68CLhdP_c
+
+# pikachu = Pokemon.create(name: 'Pikachu', poke_type: 'Electric', move: 'thunderbolt', image: 'pokemon_app/app/assets/images/pikachu.png', likes: 0)
+# bulbasaur = Pokemon.create(name: 'Bulbasaur', poke_type: 'Grass', move: 'overgrow', image: 'pokemon_app/app/assets/images/bulbasaur.png', likes: 0)
+# charmander = Pokemon.create(name: 'Charmander', poke_type: 'Fire', move: 'blaze', image: 'pokemon_app/app/assets/images/charmander.png', likes: 0)
+# Squirtle =  Pokemon.create(name: 'Squirtle', poke_type: 'Water', move: 'torrent', image: 'pokemon_app/app/assets/images/squirtle.png', likes: 0)
+# snorlax = Pokemon.create(name: 'Snorlax', poke_type: 'Normal', move: 'Gluttony', image: 'pokemon_app/app/assets/images/snorlax.png', likes: 0)
+# jigglypuf = Pokemon.create(name: 'Jigglypuff', poke_type: 'Normal', move: 'Cute Charm', image: 'pokemon_app/app/assets/images/jigglypuff.png', likes: 0)
+# abra = Pokemon.create(name: 'Abra', poke_type: 'Psychic', move: 'Magic Guard', image: 'pokemon_app/app/assets/images/abra.png', likes: 0)
+
+magikarp = Pokemon.create(name: 'Magikarp', poke_type: 'Water', move: 'Swift Swim', image: 'pokemon_app/app/assets/images/magikarp.png', likes: 0)
+haunter = Pokemon.create(name: 'Haunter', poke_type: 'Ghost', move: 'Levitate', image: 'pokemon_app/app/assets/images/haunter.png', likes: 0)
