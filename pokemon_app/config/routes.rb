171ef268b64114a4648da1fd6804d73607ec8690
jw20191n <clairejw1015@gmail.com>
2020-09-10 23:41:56 -0400

    added favicon.ico

--- pokemon_app/config/routes.rb
+++ pokemon_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'home/index'
   get '/pokemons', to: 'pokemon#index', as: :pokemons
   get '/pokemons/:id', to: 'pokemon#show'
   patch '/pokemons/:id', to: 'pokemon#update'
